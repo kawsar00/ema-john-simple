9ba3535489beb68d3220b03181df7132cda219e6
kawsar00 <kawsarmahmud358@gmail.com>
2020-08-24 22:45:21 +0600

    shop cart site coding complete

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const Cart = (props) => {
+  const cart = props.cart
+  console.log(cart)
+
+  // const total = cart.reduce((total, prd) => total + prd.price, 0)
+  let total = 0;
+  for (let i = 0; i < cart.length; i++) {
+    const product = cart[i];
+    total = total + product.price
+  }
+
+  let shipping = 0;
+  if (total > 35) {
+    shipping = 0;
+  } else if (total > 15) {
+    shipping = 4.99;
+  } else if (total > 0) {
+    shipping = 14.99;
+  }
+
+  let tax = total / 10; //tax
+
+  const grandTotal = (total + shipping + tax) //all price calculate
+  const numberFormat = num =>{
+    const precision = num.toFixed(2);
+    return Number(precision)
+  } // function for converting into Number and toFixed into 2 digit
+
+  return (
+    <div>
+      <h2>Order Summary</h2>
+      <p>Item ordered: ${cart.length}</p>
+      <p><small>Product Price: ${numberFormat(total)}</small></p>
+      <p><small>Shipping Cost: ${shipping}</small></p>
+      <p><small>Tax + VAT: ${numberFormat(tax)}</small></p>
+  <p><small>Total Price: ${numberFormat(grandTotal)}</small></p>
+    </div>
+  );
+};
+
+export default Cart;
